9bd33983ee5160ca9014252503e8469c069dfd96
mudathirmahgoub <mudathirmahgoub@gmail.com>
2024-09-10 13:12:50 -0500

    renaming

--- plot.py
+++ plot.py
@@ -1,48 +1,48 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('relations_vs_quantifiers.csv',header=0)
-relations = df["relations duration"]
+df = pd.read_csv('filters_vs_quantifiers.csv',header=0)
+filters = df["filters duration"]
 quantifiers = df["quantifiers duration"]
 
-# relations vs quantifiers
+# filters vs quantifiers
 plt.figure(0)
-plt.xlabel("cvc5 relations")
+plt.xlabel("cvc5 filters")
 plt.ylabel("cvc5 quantifiers")
-plt.title("cvc5 relations vs quantifiers")
+plt.title("cvc5 filters vs quantifiers")
 plt.grid(linestyle = '--')
 plt.axline((0, 0), slope=1, color="red")
-plt.scatter(relations, quantifiers, marker = '+')
+plt.scatter(filters, quantifiers, marker = '+')
 plt.legend(['y=x', 'seconds'])
-plt.savefig("relation_vs_quantifiers.svg", bbox_inches='tight')
+plt.savefig("filters_vs_quantifiers.svg", bbox_inches='tight')
 
 plt.figure(1)
-labels = ['cvc5 relations', 'cvc5 quantifiers']
+labels = ['cvc5 filters', 'cvc5 quantifiers']
 plt.ylabel('seconds')
-plt.title("cvc5 relations vs quantifiers")
-bplot = plt.boxplot([relations, quantifiers], labels=labels)  
+plt.title("cvc5 filters vs quantifiers")
+bplot = plt.boxplot([filters, quantifiers], labels=labels)  
 plt.savefig("cvc5_box_plot.svg", bbox_inches='tight')
 
-# relations vs z3
+# filters vs z3
 plt.figure(2)
-df = pd.read_csv('relations.csv',header=0)
-relations = df["duration"]
+df = pd.read_csv('filters.csv',header=0)
+filters = df["duration"]
 df = pd.read_csv('../LEGOS/Sleec/z3.csv',header=0)
 z3 = df["duration"]
-plt.xlabel("cvc5 relations")
+plt.xlabel("cvc5 filters")
 plt.ylabel("z3 qf formulas")
-plt.title("cvc5 relations vs z3 qf formulas")
+plt.title("cvc5 filters vs z3 qf formulas")
 plt.grid(linestyle = '--')
 plt.axline((0, 0), slope=1, color="red")
-plt.scatter(relations, z3, marker = '+')
+plt.scatter(filters, z3, marker = '+')
 plt.legend(['y=x', 'seconds'])
 plt.savefig("cvc5_vs_z3.svg", bbox_inches='tight')
 
 plt.figure(3)
-labels = ['cvc5 relations', 'z3']
+labels = ['cvc5 filters', 'z3']
 plt.ylabel('seconds')
-plt.title("cvc5 relations vs z3 qf formulas")
-bplot = plt.boxplot([relations, z3], labels=labels)  
+plt.title("cvc5 filters vs z3 qf formulas")
+bplot = plt.boxplot([filters, z3], labels=labels)  
 plt.savefig("cvc5_vs_z3_box_plot.svg", bbox_inches='tight')
 
 
