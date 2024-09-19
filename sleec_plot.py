36cb155500598fbcf137d96a30fa171edd9ca101
mudathirmahgoub <mudathirmahgoub@gmail.com>
2024-09-19 12:13:08 -0500

    cvc5 summit

--- sleec_plot.py
+++ sleec_plot.py
@@ -4,60 +4,47 @@ import common
 import cactus
 import numpy as np
 
-df = pd.read_csv("filters_vs_quantifiers.csv", header=0)
-filters = df["filters duration"]
-quantifiers = df["quantifiers duration"]
+df = pd.read_csv("filters.csv", header=0)
+filters = df["duration"]
+df = pd.read_csv("quantifiers.csv", header=0)
+quantifiers = df["duration"]
+df = pd.read_csv("../LEGOS/Sleec/z3.csv", header=0)
+z3 = df["duration"]
 
 options = common.PLOT_OPTIONS
-options["save_to"] = "cactus.svg"
+options["save_to"] = "cactus.png"
 plotter = cactus.Cactus(options)
 instances = len(filters.tolist())
 accumulative_filter = np.cumsum(filters.tolist())
-accumulative_quantifiers = np.cumsum(quantifiers.tolist())
+accumulative_z3 = np.cumsum(z3.tolist())
 data = [
-    ["filter", accumulative_filter, instances, options["timeout"]],
-    ["quantifiers", accumulative_quantifiers, instances, options["timeout"]],
+    ["cvc5 filters", accumulative_filter, instances, options["timeout"]],
+    ["LEGOS(z3)", accumulative_z3, instances, options["timeout"]],
 ]
 
 plotter.create(data)
 
 
-# filters vs quantifiers
-plt.figure(0)
-plt.xlabel("cvc5 filters")
-plt.ylabel("cvc5 quantifiers")
-plt.title("cvc5 filters vs quantifiers")
-plt.grid(linestyle="--")
-plt.axline((0, 0), slope=1, color="red")
-plt.scatter(filters, quantifiers, marker="+")
-plt.legend(["y=x", "seconds"])
-plt.savefig("filters_vs_quantifiers.svg", bbox_inches="tight")
-
-plt.figure(1)
-labels = ["cvc5 filters", "cvc5 quantifiers"]
-plt.ylabel("seconds")
-plt.title("cvc5 filters vs quantifiers")
-bplot = plt.boxplot([filters, quantifiers], labels=labels)
-plt.savefig("cvc5_box_plot.svg", bbox_inches="tight")
+# # filters vs quantifiers
+# plt.figure(0)
+# plt.xlabel("cvc5 filters")
+# plt.ylabel("cvc5 quantifiers")
+# plt.title("cvc5 filters vs quantifiers")
+# plt.grid(linestyle="--")
+# plt.axline((0, 0), slope=1, color="red")
+# plt.scatter(filters, quantifiers, marker="x")
+# plt.legend(["y=x", "seconds"])
+# plt.savefig("filters_vs_quantifiers.png", bbox_inches="tight")
 
 # filters vs z3
-plt.figure(2)
-df = pd.read_csv("filters.csv", header=0)
-filters = df["duration"]
-df = pd.read_csv("../LEGOS/Sleec/z3.csv", header=0)
-z3 = df["duration"]
-plt.xlabel("cvc5 filters")
-plt.ylabel("z3 qf formulas")
-plt.title("cvc5 filters vs z3 qf formulas")
-plt.grid(linestyle="--")
-plt.axline((0, 0), slope=1, color="red")
-plt.scatter(filters, z3, marker="+")
-plt.legend(["y=x", "seconds"])
-plt.savefig("cvc5_vs_z3.svg", bbox_inches="tight")
+# plt.figure(1)
 
-plt.figure(3)
-labels = ["cvc5 filters", "z3"]
-plt.ylabel("seconds")
-plt.title("cvc5 filters vs z3 qf formulas")
-bplot = plt.boxplot([filters, z3], labels=labels)
-plt.savefig("cvc5_vs_z3_box_plot.svg", bbox_inches="tight")
+# plt.xlabel("cvc5 filters")
+# plt.ylabel("LEGOS(z3)")
+# plt.title("cvc5 filters vs LEGOS(z3)")
+# plt.grid(linestyle="--")
+# plt.axline((0, 0), slope=1, color="red")
+# plt.scatter(filters, z3, marker="x")
+# plt.legend(["y=x", "seconds"])
+# plt.savefig("cvc5_vs_z3.png", bbox_inches="tight")
+
