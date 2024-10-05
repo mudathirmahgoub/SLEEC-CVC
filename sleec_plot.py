4b00387b7aa5378be2a60f6fe8c1fda3cf5ff54c
mudathirmahgoub <mudathirmahgoub@gmail.com>
2024-10-04 19:55:13 -0500

    after cvc5 fix

--- sleec_plot.py
+++ sleec_plot.py
@@ -11,24 +11,26 @@ quantifiers = df["duration"]
 df = pd.read_csv("../LEGOS/Sleec/z3.csv", header=0)
 z3 = df["duration"]
 
-options = common.PLOT_OPTIONS
-options["save_to"] = "cactus.png"
-plotter = cactus.Cactus(options)
-instances = len(filters.tolist())
-accumulative_filter = np.cumsum(filters.tolist())
-accumulative_z3 = np.cumsum(z3.tolist())
-data = [
-    ["cvc5 filters", accumulative_filter, instances, options["timeout"]],
-    ["LEGOS(z3)", accumulative_z3, instances, options["timeout"]],
-]
+# options = common.PLOT_OPTIONS
+# options["save_to"] = "cactus.png"
+# plotter = cactus.Cactus(options)
+# instances = len(filters.tolist())
+# accumulative_filter = np.cumsum(filters.tolist())
+# accumulative_z3 = np.cumsum(z3.tolist())
+# data = [
+#     ["cvc5 filters", accumulative_filter, instances, options["timeout"]],
+#     ["LEGOS(z3)", accumulative_z3, instances, options["timeout"]],
+# ]
 
-plotter.create(data)
+# plotter.create(data)
 
 
 # # filters vs quantifiers
 # plt.figure(0)
 # plt.xlabel("cvc5 filters")
 # plt.ylabel("cvc5 quantifiers")
+# plt.xscale('log')
+# plt.yscale('log')
 # plt.title("cvc5 filters vs quantifiers")
 # plt.grid(linestyle="--")
 # plt.axline((0, 0), slope=1, color="red")
@@ -39,12 +41,12 @@ plotter.create(data)
 # filters vs z3
 # plt.figure(1)
 
-# plt.xlabel("cvc5 filters")
-# plt.ylabel("LEGOS(z3)")
-# plt.title("cvc5 filters vs LEGOS(z3)")
-# plt.grid(linestyle="--")
-# plt.axline((0, 0), slope=1, color="red")
-# plt.scatter(filters, z3, marker="x")
-# plt.legend(["y=x", "seconds"])
-# plt.savefig("cvc5_vs_z3.png", bbox_inches="tight")
+plt.xlabel("cvc5 filters")
+plt.ylabel("LEGOS(z3)")
+plt.title("cvc5 filters vs LEGOS(z3)")
+plt.grid(linestyle="--")
+plt.axline((0, 0), slope=1, color="red")
+plt.scatter(filters, z3, marker="x")
+plt.legend(["y=x", "seconds"])
+plt.savefig("cvc5_vs_z3.png", bbox_inches="tight")
 
