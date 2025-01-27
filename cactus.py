75c8a6e28d7e182c4233c6829e7b4c8c539250b2
mudathirmahgoub <mudathirmahgoub@gmail.com>
2025-01-27 16:16:53 -0600

    markers

--- cactus.py
+++ cactus.py
@@ -2,32 +2,44 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import math
+
 # Read data from CSV file
-files = ['data1.csv','data2.csv', 'data3.csv', 'data4.csv', 'data5.csv', 'data6.csv']
-
+files = ["data1.csv", "data2.csv", "data3.csv", "data4.csv"]
+# https://matplotlib.org/stable/api/markers_api.html
+markers = ["o","*","D", "^"]
 plt.figure()
-
-for file in files:    
+i = 0
+for file in files:
 
     data = pd.read_csv(file)
 
     # List of solver names (columns after the first column)
     solvers = data.columns[1:]
-
+    
     # Sort and plot the cumulative performance with log scale for each solver
     for solver in solvers:
         performance = np.sort(data[solver])
         # cumulative_performance = np.cumsum(performance)
         cumulative_performance = performance
-        plt.step(cumulative_performance, range(1, len(cumulative_performance) + 1), label=solver)
+        plt.step(
+            cumulative_performance,
+            range(1, len(cumulative_performance) + 1),
+            label=solver,
+            marker=markers[i],
+            markersize=10,
+            linestyle="-",
+            where="post", 
+            markevery=100           
+        )
+    i = i + 1    
 
 # Set logarithmic scale
-plt.xscale('log')
+plt.xscale("log")
 # plt.yscale('log')
 
 # Set labels and title
-plt.xlabel('Time (seconds)')
-plt.ylabel('# of Instances')
+plt.xlabel("Time (seconds)")
+# plt.ylabel('# of Instances')
 # plt.title('Cactus Plot')
 
 # Add a legend
