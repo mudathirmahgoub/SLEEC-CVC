d129508c77589629cc94bf42e80428bff61b08b1
mudathirmahgoub <mudathirmahgoub@gmail.com>
2025-01-30 10:41:52 -0600

    smtlib filter

--- rename_benchmarks.py
+++ rename_benchmarks.py
@@ -1,5 +1,5 @@
 import os
-path = "test_files_set_quantifiers"
+path = "test_files_filters"
 files = [
     os.path.join(dp, f)
     for dp, _, filenames in os.walk(path)
@@ -9,8 +9,8 @@ files = [
 
 import os
 for file in files:
-  name = file.replace("/home/mudathir/Desktop/SLEEC-CVC/test_files_set_quantifiers","")
+  name = file.replace("/home/mudathir/Desktop/SLEEC-CVC/test_files_filters","")
   name = name.replace("/", "_")  
   name = name.replace("test_files_","")  
   print (name)
-  os.rename(file, "smtlib_set_quantifiers/{}".format(name))
+  os.rename(file, "smtlib_filters/{}".format(name))
